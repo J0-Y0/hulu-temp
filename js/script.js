70933738971827710027d44e502497c892a9fe2d
J0-Y0 <yosfemyayu@gmail.com>
2022-11-26 11:29:02 -0800

    the first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//for multi stage drop down menu of location selection 
+$(document).ready(function(){
+    $('.dropdown-submenu a.test').on("click", function(e){
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
